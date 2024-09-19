e0b2de7414ed98e1f9fa61f9e25da62fb5d4add9
carlostorres970 <luis.oliveros1@udea.edu.co>
2024-09-19 10:38:34 -0500

    Update TrabajoFinal.py

--- src/TrabajoFinal.py
+++ src/TrabajoFinal.py
@@ -2,6 +2,54 @@
 # @markdown
 # @markdown Doble clic en mí y podrás ver mi código.
 # =====================================================================================================================
+# REGION: Congiguración de la consola
+# =====================================================================================================================
+from rich.console import Console
+from rich.theme import Theme
+from rich.progress import Progress
+TemaAnalisisDatos = Theme({
+    "info": "bold navy_blue on grey82",
+    "url": "underline blue",
+    "warning": "dark_orange",
+    "danger": "bold red",
+    "normalb": "bold black",
+    "normal": "black"
+}, inherit=False)
+console = Console(theme=TemaAnalisisDatos)
+# =====================================================================================================================
+# REGION: Librerías
+# =====================================================================================================================
+with console.status("Cargando Librerías", spinner="clock"):
+    import os
+    import random as rnd
+    import pandas as pd
+    import time
+    import logging
+    import tqdm
+    import rich
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import statistics as st
+    import math as mt
+    # from google.colab import drive
+    import sys
+    import datetime
+    import time
+    from tqdm import tqdm
+    #import ace_tools as tools
+    from collections import Counter
+    inicio = time.time()
+    import warnings
+    # Ignorar todos los warnings
+    warnings.filterwarnings("ignore")
+# =====================================================================================================================
+# REGION: Librerías
+# =====================================================================================================================
+console.print("Librerías cargadas satisfactoriamente", style="info")
+# @markdown ## **Ejecutar esta celda (►)**
+# @markdown
+# @markdown Doble clic en mí y podrás ver mi código.
+# =====================================================================================================================
 # REGION: Funciones Útiles para Taller Proyecto Integrador
 # NOTA: Las funciones fueron comentadas con GitHub Copilot
 # =====================================================================================================================
